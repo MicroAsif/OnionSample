c6a75bc6612f8a0ed72eafe62650bc58a350d312
Asif Rahman <asif0531@live.com>
2019-02-12 01:20:17 +0600

    cros and api updated

---
+++ OnionSample/Onion.Web/Controllers/CompanyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OnionSample.Domain.Interfaces.Repository;
+
+namespace Onion.Web.Controllers
+{
+    public class CompanyController : ApiController
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompanyController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(_companyRepository.All());
+        }
+    }
+}
--- OnionSample/Onion.Web/Controllers/MedicineController.cs
+++ OnionSample/Onion.Web/Controllers/MedicineController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Services.Protocols;
+using OnionSample.Domain.Entity;
 using OnionSample.Domain.Interfaces.Business;
 using OnionSample.Domain.Interfaces.Repository;
 
@@ -23,7 +27,15 @@ namespace Onion.Web.Controllers
         [HttpGet]
         public IHttpActionResult Get()
         {
-            return Ok(_medicineRepository.All().ToList());
+            return Ok(_medicineRepository.All().Include(x=>x.Company).ToList());
+        }
+        [HttpPost]
+        public async Task<IHttpActionResult> Post(Medicine model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            await _medicineRepository.Insert(model);
+            return Ok(model);
         }
     }
 }
